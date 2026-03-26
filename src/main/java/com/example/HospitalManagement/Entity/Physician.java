3e08637582e5cf896217404da4e0e8a5c290bd34
Nikhil Gupta <nikhilgupta932003@gmail.com>
2026-03-26 10:39:49 +0530

    refactor: Replace Lombok annotations with explicit getters, setters, and constructors in Physician entity

--- src/main/java/com/example/HospitalManagement/Entity/Physician.java
+++ src/main/java/com/example/HospitalManagement/Entity/Physician.java
@@ -6,9 +6,7 @@ import lombok.*;
 import java.util.List;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Table(name = "physician")
 public class Physician {
 
@@ -25,5 +23,45 @@ public class Physician {
     @Column(nullable = false, unique = true)
     private int ssn;
 
-    
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public Physician(int employeeId, String name, String position, int ssn) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.position = position;
+        this.ssn = ssn;
+    }
+
+    public Physician() {
+    }
 }
